d4b2f88ba6f270c18834c05577ee108cc42f460c
Dirga Yasa <cekingx@gmail.com>
2025-04-11 12:02:06 +0800

    feat: climbing stairs

--- src/70-climbing-stairs.ts
+++ src/70-climbing-stairs.ts
@@ -1,5 +1,30 @@
 function climbStairs(n: number): number {
-  return 0;    
+  if (n < 1) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else if (n == 2) {
+    return 2;
+  } else {
+    return climbStairs(n-1) + climbStairs(n-2);
+  }
 };
 
-export default climbStairs;
+function climbStairsIter(n: number): number {
+  if (n < 1) return 0;
+  if (n == 1) return 1;
+  if (n == 2) return 2;
+
+  let prev1 = 1;
+  let prev2 = 2;
+
+  for(let i = 3; i <= n; i++) {
+    const current = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = current;
+  }
+
+  return prev2
+}
+
+export { climbStairs, climbStairsIter };
--- test/70-climbing-stairs.spec.ts
+++ test/70-climbing-stairs.spec.ts
@@ -1,10 +1,38 @@
 import { describe, it, assert } from 'vitest';
-import climbStairs from '../src/70-climbing-stairs';
+import { climbStairs, climbStairsIter } from '../src/70-climbing-stairs';
 
 describe("70-climbing-stairs", function () {
-  it("should return 0", () => {
-    const result = climbStairs(1);
+  describe("Recursion", () => {
+    it("should return value for 1 step", () => {
+      const result = climbStairs(1);
+      assert.equal(result, 1);
+    })
 
-    assert.equal(result, 0);
+    it("should return value for 5 step", () => {
+      const result = climbStairs(5);
+      assert.equal(result, 8)
+    })
+
+    it("should return value for 44 step", () => {
+      const result = climbStairs(44);
+      assert.equal(result, 1134903170)
+    })
+  })
+
+  describe("Iteration", () => {
+    it("should return value for 1 step", () => {
+      const result = climbStairsIter(1);
+      assert.equal(result, 1)
+    })
+
+    it("should return value for 5 step", () => {
+      const result = climbStairs(5);
+      assert.equal(result, 8)
+    })
+
+    it("should return value for 44 step", () => {
+      const result = climbStairs(44);
+      assert.equal(result, 1134903170)
+    })
   })
 })
